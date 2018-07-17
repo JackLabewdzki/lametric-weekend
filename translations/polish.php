2b2af5d9eeb9bf6a05111d82778baec17493ac81
JackLabewdzki <41347701+JackLabewdzki@users.noreply.github.com>
2018-07-18 00:03:31 +0200

    Create polish.php

---
+++ translations/polish.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'd88d6b865ab77786075f28c80b293156' => 'Prawie, ale jeszcze nie teraz :(',
+    'a1386a1de1a4d2efbff582b2d2874b2c' => 'Wkrótce....',
+    '10e4e3a9b10056c177e3f4c131533f23' => 'Jest weekend! \o/',
+    '3accdebf72a75061a33b3bd33d54a28a' => 'Oficjalnie nie, ale jakby był.',
+    'cbdf2122065fb2a68b1c14fd82482810' => 'To jest koniec :(',
+    'c215c811b8791e49ccd39c177f58bd8d' => 'No i tracimy wakacje w ten weekend :(',
+    '3ab2f6f6b038e237765335ad0cea43fc' => 'Ale jutro... nie pracujemy! \o/',
+    '98ed3a487cf9f80c10bdafe2189fc531' => 'Właściwie to tak, to jest weekend \o/',
+    '7514b1a515e6b5494236f32e24d34b6d' => 'Ale nie pracujemy! \o/',
+    '62d17a9dfd12f3ec8817c58a81b38669' => 'Nie.'
+];
